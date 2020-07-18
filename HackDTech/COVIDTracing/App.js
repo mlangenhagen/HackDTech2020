65b031d82cb37eeaff8e96e0dc3d58478c7e62a6
GLalita <glp16@duke.edu>
2020-07-18 17:29:05 -0400

    Grace init commit

--- HackDTech/COVIDTracing/App.js
+++ HackDTech/COVIDTracing/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing Hello World!</Text>
+      <Text>Grace's Test!</Text>
       <StatusBar style="auto" />
     </View>
   );
